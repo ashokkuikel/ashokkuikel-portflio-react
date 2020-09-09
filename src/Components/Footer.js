d9ca6b3b2e221dfa1580c14c5a123f2e50adae43
ashokkuikel <mail.ashokkuikel@gmail.com>
2020-09-09 10:10:01 +0545

    initial update

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,12 +19,12 @@ class Footer extends Component {
            </ul>
 
            <ul>
-            <li>Modified with love by the Clever Programmer team ♥️</li>
+            <li class="text-white">&copy; 2017 -2020 Ashok Kuikel</li>
            </ul>
 
            <ul className="copyright">
-              <li>&copy; All credit goes to Tim Baker - Copyright 2017 Tim Baker</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              {/* <li>&copy; All credit goes to Tim Baker - Copyright 2017 Tim Baker</li> */}
+              {/* <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li> */}
            </ul>
 
         </div>
